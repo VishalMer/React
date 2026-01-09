3ce85eb39484dba7c47ef564de1b81a0f4db0c27
VishalMer <vishalmer99747@gmail.com>
2026-01-09 17:22:37 +0530

    Types of CSS - used module css - installed & learned tailwind css

---
+++ css/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Button from './styles/button/button'
+import Header from './styles/header/header'
+const App = () => {
+  return (
+    <div>
+      <Header />
+      <Button />
+    </div>
+  )
+}
+
+export default App
