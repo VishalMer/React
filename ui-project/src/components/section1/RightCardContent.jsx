400995efa0dc6a3f169859e02c3145fdec0fd0aa
VishalMer <vishalmer99747@gmail.com>
2026-01-10 18:35:15 +0530

    refactor: solved a wrning by adding css in main file and removed from the classname

--- ui-project/src/components/section1/RightCardContent.jsx
+++ ui-project/src/components/section1/RightCardContent.jsx
@@ -5,7 +5,7 @@ const RightCardContent = (props) => {
         <div className='absolute h-full w-full top-0 left-0 p-6 flex flex-col justify-between'>
             <div>
                 <h3 style={{color:props.color}} className='bg-white flex justify-center rounded-full font-semibold h-8 w-8 items-center'>{props.id}</h3>
-                <p className='text-l text-white mt-6 leading-4 [text-shadow:_0_2px_2px_rgb(0_0_0_/_0.9)]'>{props.intro}</p>
+                <p id="intro" className='text-l text-white mt-6 leading-4'>{props.intro}</p>
             </div>
             <div>
                 <div className='flex justify-between'>
